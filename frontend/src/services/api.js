1beaa59fd0650ba7f2eafc9abb089694078139f3
Sephyros <leo037br@hotmail.com>
2020-12-16 05:45:53 -0300

    Updated ip

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 const api = axios.create({
-  baseURL: 'http://192.168.0.24:3001'
+  baseURL: "http://192.168.0.15:3001",
 });
 
 export default api;
